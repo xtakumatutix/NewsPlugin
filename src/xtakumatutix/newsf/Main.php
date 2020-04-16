1d2b33e9caf7f8fa4d4e445d20a7b2cfaa7bf015
xtakumatutix <47268002+xtakumatutix@users.noreply.github.com>
2020-04-17 02:30:50 +0900

    Add files via upload

---
+++ src/xtakumatutix/newsf/Main.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace xtakumatutix\newsf;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use RuinPray\ui\UI;
+use RuinPray\ui\forms\SimpleForm;
+
+Class Main extends PluginBase implements Listener {
+
+    public function onEnable(){
+        $this->getLogger()->notice("読み込み完了_ver.1.0.0");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->AlreadyRead = new Config($this->getDataFolder() . "AlreadyRead.yml", Config::YAML);
+        $this->News = new Config($this->getDataFolder() . "News.yml", Config::YAML, array(
+            'ニュース' => 'Hello World{br}改行だよ＞＜',
+        ));
+    }
+
+	public function onJoin(PlayerJoinEvent $event)
+	{
+		$player = $event->getPlayer();
+		$name = $player->getName();
+		$message =$this->News->get("ニュース");
+    	$message = str_replace("{br}", "\n", $message);
+		if(!$this->AlreadyRead->get($name) == null) {
+		}else{
+		    $this->AlreadyRead->set($name);
+            $this->AlreadyRead->save();
+            $form = UI::createSimpleForm(100);
+		    $form->setTitle("News");
+		    $form->setContent("".$message."");
+		    $form->addButton("§c閉じる");   
+		    UI::sendForm($player, $form);
+		}
+        return true;
+	}
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args):bool{
+		if ($sender instanceof Player) {
+    		$sender->sendMessage("§cプレイヤーにしかformを送れないよ！");
+    	} else {
+			$player = $sender;
+		    $message =$this->News->get("ニュース");
+    	    $message = str_replace("{br}", "\n", $message);
+            $form = UI::createSimpleForm(100);
+		    $form->setTitle("News");
+		    $form->setContent("".$message."");
+		    $form->addButton("§c閉じる");   
+		    UI::sendForm($player, $form);
+    	}
+    	return true;
+    }
+}
